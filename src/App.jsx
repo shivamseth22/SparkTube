1d22ac24212fa83c4cfeb078daa601a49502d6d7
Shivam seth <shivamseth221104@gmail.com>
2023-06-17 10:50:33 +0530

    SparkTube

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = ()=> {
+ 
+
+  return (
+    <>
+      <div className='bg-red-400'>hiii</div>
+       
+    </>
+  )
+}
+
+export default App;
